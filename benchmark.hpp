070d8111593263f6d64d9c2e76b0cac3ca6031d7
Bojan Faletic <bojanf.1996@gmail.com>
2021-07-14 16:31:25 +0200

    removed define in header

--- benchmark.hpp
+++ benchmark.hpp
@@ -5,9 +5,5 @@
 #include <iostream>
 #include <stdint.h>
 
-using nano_s = std::chrono::nanoseconds;
-using time_s = std::chrono::high_resolution_clock;
-#define N_TIMES 1024
-
 template <typename T> uint32_t time_diff(T F);
 void benchmark(matrix m1);
