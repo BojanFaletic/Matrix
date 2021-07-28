34ebfbaf026fe13aba433d50e8013203dadd5fe7
Bojan Faletic <bojanf.1996@gmail.com>
2021-07-28 19:35:01 +0200

    matrix4 builds

--- header/matrix4.hpp
+++ header/matrix4.hpp
@@ -1,47 +1,38 @@
 #pragma once
 
-#include "matrix4.hpp"
+#include "matrix_generic.hpp"
 #include <sstream>
 #include <stdint.h>
 #include <vector>
 
-class matrix4 {
-public:
-  uint32_t n, m, z, y;
-  float *mat;
+struct matrix4 : public matrix_generic {
+  using matrix_generic::size;
 
   void size(uint32_t y, uint32_t z, uint32_t n, uint32_t m);
-  uint32_t idx(uint32_t y, uint32_t z, uint32_t n, uint32_t m) const;
-
-public:
   matrix4();
   matrix4(matrix4 const &m);
-  ~matrix4();
+  matrix4(matrix_generic const &m);
+  matrix4(uint32_t const z, uint32_t const y, uint32_t const n,
+          uint32_t const m);
+  matrix4(float *data, uint32_t const z, uint32_t const y, uint32_t const n,
+          uint32_t const m);
   matrix4(
       std::vector<std::vector<std::vector<std::vector<float>>>> const &in_mat);
+  ~matrix4();
 
   static matrix4 zeros(uint32_t y, uint32_t z, uint32_t n, uint32_t m);
   static matrix4 ones(uint32_t y, uint32_t z, uint32_t n, uint32_t m);
   static matrix4 random(uint32_t y, uint32_t z, uint32_t n, uint32_t m);
+
   // take index of matrix
   float &operator()(uint32_t const y, uint32_t const z, uint32_t const n,
                     uint32_t const m);
   float operator()(uint32_t const y, uint32_t const z, uint32_t const n,
                    uint32_t const m) const;
-  void operator=(matrix4 const &m);
-  uint32_t size() const;
+  void operator=(matrix_generic const &m);
+
+  std::array<uint32_t, 4> shape() const;
+  uint32_t shape(uint32_t const axis) const;
 };
 
 std::ostream &operator<<(std::ostream &out, matrix4 const &M);
-
-// scalar operators on matrix
-matrix4 operator*(matrix4 const &M, float n);
-matrix4 operator/(matrix4 const &M, float n);
-matrix4 operator+(matrix4 const &M, float n);
-matrix4 operator-(matrix4 const &M, float n);
-matrix4 operator*=(matrix4 &M, float n);
-matrix4 operator/=(matrix4 &M, float n);
-matrix4 operator+=(matrix4 &M, float n);
-matrix4 operator-=(matrix4 &M, float n);
-// operation on matrix
-matrix4 operator*(matrix4 const &M, matrix4 const &C);
--- src/matrix4.cpp
+++ src/matrix4.cpp
@@ -6,33 +6,26 @@
 #include <sstream>
 
 void matrix4::size(uint32_t y, uint32_t z, uint32_t n, uint32_t m) {
-  this->n = n;
-  this->m = m;
-  this->z = z;
-  this->y = y;
+  dim = {y, z, n, m};
 }
 
-uint32_t matrix4::idx(uint32_t y, uint32_t z, uint32_t n, uint32_t m) const {
-  uint32_t layer = n * this->m + m;
-  uint32_t i3 = layer * this->z + z;
-  return i3 * this->y + y;
-}
-
-uint32_t matrix4::size() const { return m * n * z * y; }
+uint32_t matrix4::shape(uint32_t const axis) const { return dim[axis]; }
 
 matrix4::matrix4() {
-  n = 0;
-  m = 0;
-  z = 0;
-  y = 0;
+  size(0, 0, 0, 0);
   mat = nullptr;
 }
 
 matrix4::matrix4(matrix4 const &m) {
-  this->size(m.y, m.z, m.n, m.m);
+  size(m.shape(0), m.shape(1), m.shape(2), m.shape(3));
   mat = new float[size()];
-  uint32_t it = 0;
-  std::for_each(mat, mat + this->size(), [&](float &f) { f = m.mat[it++]; });
+  copy(m);
+}
+
+matrix4::matrix4(matrix_generic const &m) {
+  size(m.shape(0), m.shape(1), m.shape(2), m.shape(3));
+  mat = new float[size()];
+  copy(m);
 }
 
 matrix4::~matrix4() {
@@ -42,10 +35,14 @@ matrix4::~matrix4() {
 
 matrix4::matrix4(
     std::vector<std::vector<std::vector<std::vector<float>>>> const &in_mat) {
-  this->size(in_mat.size(), in_mat[0].size(), in_mat[0][0].size(),
-             in_mat[0][0][0].size());
+  uint32_t y = in_mat.size();
+  uint32_t z = in_mat[0].size();
+  uint32_t n = in_mat[0][0].size();
+  uint32_t m = in_mat[0][0][0].size();
 
+  this->size(y, z, n, m);
   mat = new float[size()];
+
   uint32_t it = 0;
   for (std::vector<std::vector<std::vector<float>>> const &sub_v2 : in_mat) {
     for (std::vector<std::vector<float>> const &sub_v : sub_v2) {
@@ -58,12 +55,11 @@ matrix4::matrix4(
   }
 }
 
-void matrix4::operator=(matrix4 const &m) {
-  this->size(m.y, m.z, m.m, m.n);
+void matrix4::operator=(matrix_generic const &m) {
+  this->size(m.shape(0), m.shape(1), m.shape(2), m.shape(3));
   delete[] mat;
   mat = new float[size()];
-  uint32_t it = 0;
-  std::for_each(mat, mat + this->size(), [&](float &f) { f = m.mat[it++]; });
+  copy(m);
 }
 
 matrix4 matrix4::zeros(uint32_t y, uint32_t z, uint32_t n, uint32_t m) {
@@ -98,68 +94,6 @@ matrix4 matrix4::random(uint32_t y, uint32_t z, uint32_t n, uint32_t m) {
   return M;
 }
 
-/******************************************************************************/
-/***************************** Operators **************************************/
-/******************************************************************************/
-matrix4 operator*(matrix4 const &M, float n) {
-  matrix4 A = M;
-  std::for_each(A.mat, A.mat + A.size(), [&](float &f) { f *= n; });
-  return A;
-}
-
-matrix4 operator*=(matrix4 &M, float n) {
-  std::for_each(M.mat, M.mat + M.size(), [&](float &f) { f *= n; });
-  return M;
-}
-
-matrix4 operator/=(matrix4 &M, float n) {
-  std::for_each(M.mat, M.mat + M.size(), [&](float &f) { f /= n; });
-  return M;
-}
-
-matrix4 operator/(matrix4 const &M, float n) {
-  matrix4 A = M;
-  std::for_each(A.mat, A.mat + A.size(), [&](float &f) { f /= n; });
-  return A;
-}
-
-matrix4 operator-(matrix4 const &M, float n) {
-  matrix4 A = M;
-  std::for_each(A.mat, A.mat + A.size(), [&](float &f) { f -= n; });
-  return A;
-}
-
-matrix4 operator+(matrix4 const &M, float n) {
-  matrix4 A = M;
-  std::for_each(A.mat, A.mat + A.size(), [&](float &f) { f += n; });
-  return A;
-}
-
-matrix4 operator+=(matrix4 &M, float n) {
-  std::for_each(M.mat, M.mat + M.size(), [&](float &f) { f += n; });
-  return M;
-}
-
-matrix4 operator-=(matrix4 &M, float n) {
-  std::for_each(M.mat, M.mat + M.size(), [&](float &f) { f -= n; });
-  return M;
-}
-
-matrix4 operator*(matrix4 const &M, matrix4 const &C) {
-  std::array<uint32_t, 4> sz_A{M.y, M.z, M.n, M.m};
-  std::array<uint32_t, 4> sz_B{M.y, M.z, M.n, M.m};
-
-  if (sz_A != sz_B) {
-    std::cerr << "Hadamard product unable matrix4 shape not valid\n";
-    exit(1);
-  }
-
-  matrix4 A = M;
-  uint32_t idx = 0;
-  std::for_each(A.mat, A.mat + A.size(), [&](float &f) { f *= C.mat[idx++]; });
-  return A;
-}
-
 float &matrix4::operator()(uint32_t const y, uint32_t const z, uint32_t const n,
                            uint32_t const m) {
   return mat[idx(y, z, n, m)];
@@ -173,22 +107,22 @@ float matrix4::operator()(uint32_t const y, uint32_t const z, uint32_t const n,
 std::ostream &operator<<(std::ostream &out, matrix4 const &M) {
   out << std::setprecision(2);
   out << "[";
-  for (uint32_t y = 0; y < M.y; y++) {
+  for (uint32_t y = 0; y < M.shape(0); y++) {
     out << "[";
-    for (uint32_t z = 0; z < M.z; z++) {
+    for (uint32_t z = 0; z < M.shape(1); z++) {
       out << ((z == 0) ? "[" : "  [");
-      for (uint32_t n = 0; n < M.n; n++) {
+      for (uint32_t n = 0; n < M.shape(2); n++) {
         out << ((n == 0) ? "[" : "   [");
-        for (uint32_t m = 0; m < M.m; m++) {
+        for (uint32_t m = 0; m < M.shape(3); m++) {
           out << M(y, z, n, m) << " ";
         }
-        if (z == M.z - 1 && y == M.y - 1 && n == M.n - 1) {
+        if (z == M.shape(1) - 1 && y == M.shape(0) - 1 && n == M.shape(2) - 1) {
           out << "]]]\n";
           return out;
         }
-        out << ((n == M.n - 1) ? "]" : "]\n");
+        out << ((n == M.shape(2) - 1) ? "]" : "]\n");
       }
-      out << ((z == M.z - 1) ? "]" : "]\n");
+      out << ((z == M.shape(1) - 1) ? "]" : "]\n");
     }
     out << "]\n";
   }
